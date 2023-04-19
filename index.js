a61dd8717510fa2eb6bac5f4b1f818079b28143e
Noah <noahhughesph@gmail.com>
2023-04-18 22:03:24 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee ={
+    name : '',
+    address : '',
+}
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    const newEmployee = {...employee}
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    const deleteKey = {...employee}
+    delete deleteKey[key];
+    return deleteKey;
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+}
